69452404c4ce6bff7177404987edb6d0e35d9da5
Gore Liu <goreliu@126.com>
2016-08-20 00:14:55 +0800

    Move build.bat to src

--- src/build.bat
+++ src/build.bat
@@ -1,6 +1,5 @@
 set PATH=%PATH%;c:\windows\Sysnative
 
-cd src
 bash -c ./prepare.sh
 build\ahk2exe\Ahk2Exe.exe /in run-wsl-file.ahk /out run-wsl-file.exe /icon %LOCALAPPDATA%\lxss\bash.ico
 build\ahk2exe\Ahk2Exe.exe /in open-wsl.ahk /out open-wsl.exe
