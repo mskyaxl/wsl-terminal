f4cb5f9af26c9f35ded7bee7377182f1455a5781
Gore Liu <goreliu@126.com>
2016-08-20 13:49:53 +0800

    Fix runas.js add-open-wsl-here-menu.js remove-open-wsl-here-menu.js for path with spaces

--- src/tools/add-open-wsl-here-menu.js
+++ src/tools/add-open-wsl-here-menu.js
@@ -1,8 +1,8 @@
 var WshShell = new ActiveXObject("WScript.Shell");
 
 if (WScript.Arguments.length == 0) {
-    WshShell.CurrentDirectory = ".."
-    WshShell.Run("tools\\runas.js wscript " + WScript.ScriptFullName + " " + WshShell.CurrentDirectory + "\\open-wsl.exe")
+    WshShell.CurrentDirectory = "..";
+    WshShell.Run("tools\\runas.js wscript \"" + WScript.ScriptFullName + "\" \"" + WshShell.CurrentDirectory + "\\open-wsl.exe\"");
 } else {
     WshShell.RegWrite("HKCR\\Directory\\Background\\shell\\open-wsl\\", "Open WSL Here", "REG_SZ");
     WshShell.RegWrite("HKCR\\Directory\\Background\\shell\\open-wsl\\command\\", WScript.Arguments(0), "REG_SZ");
--- src/tools/runas.js
+++ src/tools/runas.js
@@ -7,7 +7,7 @@ if (WScript.Arguments.length == 0) {
 
 var cmdArg = "";
 for (i = 1; i < WScript.Arguments.length; i++) {
-   cmdArg += WScript.Arguments(i) + " ";
+   cmdArg += "\"" + WScript.Arguments(i) + "\" ";
 }
 
 // WScript.Echo(WScript.Arguments(0) + cmdArg);
