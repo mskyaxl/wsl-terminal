446a7836609b4cb84cda3d0ef42465f87d32e1ad
Gore Liu <goreliu@126.com>
2016-08-25 08:13:08 +0800

    Build a zip package in package.sh

--- src/package.sh
+++ src/package.sh
@@ -12,6 +12,8 @@ rm -f ../*.exe
 cd ..
 rm -f ../wsl-terminal-${version}.7z
 7z a ../wsl-terminal-${version}.7z wsl-terminal
+rm -f ../wsl-terminal-${version}.zip
+7z a ../wsl-terminal-${version}.zip wsl-terminal
 
 cp *.ahk wsl-terminal
 rm -rf ../output && mv wsl-terminal ../output
