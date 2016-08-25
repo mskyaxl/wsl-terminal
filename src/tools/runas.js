c1289150c80ec8eadb7b18578d0ab62378e2e572
Gore Liu <goreliu@126.com>
2016-08-25 17:08:10 +0800

    Remove comments in runas.js

--- src/tools/runas.js
+++ src/tools/runas.js
@@ -1,4 +1,4 @@
-var UAC = new ActiveXObject("Shell.Application");
+var Shell = new ActiveXObject("Shell.Application");
 
 if (WScript.Arguments.length == 0) {
     WScript.Echo("Usage:\n    " + WScript.ScriptName + " command args");
@@ -10,9 +10,4 @@ for (i = 1; i < WScript.Arguments.length; i++) {
    cmdArg += "\"" + WScript.Arguments(i) + "\" ";
 }
 
-// WScript.Echo(WScript.Arguments(0) + cmdArg);
-// WshShell= new ActiveXObject("WScript.Shell");
-// WScript.Echo(WshShell.CurrentDirectory);
-
-
-UAC.ShellExecute(WScript.Arguments(0), cmdArg, "", "runas", 1);
+Shell.ShellExecute(WScript.Arguments(0), cmdArg, "", "runas", 1);
