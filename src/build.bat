3aca430b0f5d3d6c9a17551a5572cdd35a4cfd24
Gore Liu <goreliu@126.com>
2016-10-24 01:03:19 +0800

    Remove build.sh

--- src/build.bat
+++ src/build.bat
@@ -1,10 +1,10 @@
 set PATH=%PATH%;c:\windows\Sysnative
 
-bash -c ./prepare.sh && ^
+bash -c ./prepare.sh < build.bat && ^
 build\ahk\Compiler\Ahk2Exe.exe /in run-wsl-file.ahk /out run-wsl-file.exe /icon icons\script.ico && ^
 build\ahk\Compiler\Ahk2Exe.exe /in open-wsl.ahk /out open-wsl.exe /icon icons\terminal.ico && ^
 build\ahk\Compiler\Ahk2Exe.exe /in vim.ahk /out vim.exe /icon icons\text.ico && ^
-bash -c ./package.sh && ^
+bash -c ./package.sh < build.bat && ^
 echo Build succeeded. && ^
 exit
 
