df05177becae4259f7863116a1d388253bb40b8a
Gore Liu <goreliu@126.com>
2016-12-10 10:34:03 +0800

    Upgrade mintty and cygwin

--- src/package.sh
+++ src/package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="0.6.3"
+version="0.6.4"
 
 set -e
 
--- src/prepare.sh
+++ src/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cygwin_version="2.6.0-1"
-mintty_version="2.7.0-0"
+cygwin_version="2.6.1-0.1"
+mintty_version="2.7.1-0"
 wslbridge_version="0.2.1"
 cbwin_version="0.12"
 
