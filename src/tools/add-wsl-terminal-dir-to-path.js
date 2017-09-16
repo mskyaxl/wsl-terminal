d2ad91792357db732242c01963a7e4dab5ea758f
Gore Liu <goreliu@126.com>
2017-09-16 23:34:25 +0800

    Fix add-wsl-terminal-dir-to-path.js

--- src/tools/add-wsl-terminal-dir-to-path.js
+++ src/tools/add-wsl-terminal-dir-to-path.js
@@ -4,15 +4,16 @@ WshShell.CurrentDirectory = "..";
 
 var path = WshShell.RegRead("HKEY_CURRENT_USER\\Environment\\Path");
 
-if (path.indexOf(WshShell.CurrentDirectory) > 0) {
-    WScript.Echo(WshShell.CurrentDirectory + " is already in Path.");
+if (path.indexOf(WshShell.CurrentDirectory) >= 0) {
+    WScript.Echo(WshShell.CurrentDirectory + " is already in Path.\n\nPath:\n\n" + path);
     WScript.Quit();
 }
 
-if (path != "") {
+WScript.Echo(path);
+if (path.charAt(path.length - 1) != ";") {
     path += ";"
 }
 path += WshShell.CurrentDirectory;
 
-WshShell.Run("setx Path " + path, 0);
+WshShell.Run("setx Path \"" + path + "\"", 0);
 WScript.Echo("OK\n\nCurrent Path:\n\n" + path);
