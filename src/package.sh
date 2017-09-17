d1a6239526130638f476c7fbe9a5352e54d44339
Gore Liu <goreliu@126.com>
2017-09-17 10:08:24 +0800

    Change package.sh for debugging

--- src/package.sh
+++ src/package.sh
@@ -4,13 +4,23 @@ version="$(cat ../VERSION)"
 
 set -e
 
-rm -rf wsl-terminal && mkdir -p wsl-terminal && cd wsl-terminal
+[[ -e .debug ]] && {
+    mkdir -p wsl-terminal
+    cd wsl-terminal
+    cp -r ../build/{bin,etc} .
+    cp -r ../{*.exe,tools,cmdtool} ../../VERSION .
+    exit
+}
+
+rm -rf wsl-terminal
+mkdir -p wsl-terminal
+cd wsl-terminal
 cp -r ../build/{bin,etc,doc} .
 cp -r ../{*.exe,tools,cmdtool} ../../VERSION .
 cp -r ../../{LICENSE,README.md} doc/
 rm -f ../*.exe
-
 cd ..
+
 rm -f ../wsl-terminal-${version}.7z
 7z a ../wsl-terminal-${version}.7z wsl-terminal
 rm -f ../wsl-terminal-${version}.zip
