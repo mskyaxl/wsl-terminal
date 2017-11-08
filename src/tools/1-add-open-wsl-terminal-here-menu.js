03efaf77698983a63d2e3d1a3e724267bf0f4a39
Lukas Schneider <lukas.schneider.1990@outlook.com>
2017-11-08 01:26:25 +0100

    Fixed indent

--- src/tools/1-add-open-wsl-terminal-here-menu.js
+++ src/tools/1-add-open-wsl-terminal-here-menu.js
@@ -5,7 +5,7 @@ WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\shell\\open-wsl\\", "Open
 WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\shell\\open-wsl\\Icon", WshShell.CurrentDirectory + "\\open-wsl.exe", "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\shell\\open-wsl\\command\\"
     , WshShell.CurrentDirectory + "\\open-wsl.exe -W \"%V\"", "REG_SZ");
-    WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\Background\\shell\\open-wsl\\", "Open wsl-terminal Here", "REG_SZ");
-    WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\Background\\shell\\open-wsl\\Icon", WshShell.CurrentDirectory + "\\open-wsl.exe", "REG_SZ");
+WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\Background\\shell\\open-wsl\\", "Open wsl-terminal Here", "REG_SZ");
+WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\Background\\shell\\open-wsl\\Icon", WshShell.CurrentDirectory + "\\open-wsl.exe", "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\Directory\\Background\\shell\\open-wsl\\command\\"
     , WshShell.CurrentDirectory + "\\open-wsl.exe", "REG_SZ");
