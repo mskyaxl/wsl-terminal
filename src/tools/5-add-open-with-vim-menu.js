33febaa90d5f93fab28ad74eba707238cb859b3d
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-03-11 12:00:06 +0100

    Use string literals for paths in context menu entries

--- src/tools/5-add-open-with-vim-menu.js
+++ src/tools/5-add-open-with-vim-menu.js
@@ -4,6 +4,6 @@ WshShell.CurrentDirectory = "..";
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-terminal\\"
     , "Open with &vim in wsl-terminal", "REG_SZ");
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-terminal\\icon"
-    , WshShell.CurrentDirectory + "\\vim.exe" );
+    , "\"" + WshShell.CurrentDirectory + "\\vim.exe\"" );
 WshShell.RegWrite("HKCU\\Software\\Classes\\*\\shell\\vim-in-wsl-terminal\\command\\"
-    , WshShell.CurrentDirectory + "\\vim.exe \"%1\"", "REG_SZ");
+    , "\"" + WshShell.CurrentDirectory + "\\vim.exe\" \"%1\"", "REG_SZ");
